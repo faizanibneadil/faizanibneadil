fb688dd60be27ca844bd74d7db9e29ad5bd4c9a3
faizanibneadil <107530101+faizanibneadil@users.noreply.github.com>
2024-07-21 17:25:26 +0500

    initialize project

---
+++ providers/index.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { ThemeProvider } from "next-themes";
+
+export default function Providers({ children }: React.PropsWithChildren) {
+  return <ThemeProvider enableSystem>{children}</ThemeProvider>;
+}
