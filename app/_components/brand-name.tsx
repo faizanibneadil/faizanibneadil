2385c070c812ba7729a6e95b41aa7d3da1f5c817
faizanibneadil <107530101+faizanibneadil@users.noreply.github.com>
2024-07-22 22:09:17 +0500

    menu icon fix

--- app/_components/brand-name.tsx
+++ app/_components/brand-name.tsx
@@ -1,14 +1,14 @@
-import { Lobster } from "next/font/google";
+import { Sacramento } from "next/font/google";
 import Link from "next/link";
 
-const lora = Lobster({
+const sacramento = Sacramento({
   subsets: ["latin"],
   weight: ["400"],
 });
 export default function BrandName() {
   return (
-    <h1 className={lora.className}>
-      <Link href="/" className="text-2xl">
+    <h1 className={sacramento.className}>
+      <Link href="/" className="text-3xl font-thin">
         Faizan Adil
       </Link>
     </h1>
--- app/_components/navigation.tsx
+++ app/_components/navigation.tsx
@@ -3,13 +3,14 @@
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { cn } from "@/lib/utils";
-import { Package2 } from "lucide-react";
+import { Menu, Package2 } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import BrandName from "./brand-name";
 import ThemeSwitcher from "./theme-switcher";
 
 const menus = [
+  { name: "Home", link: "/" },
   { name: "Projects", link: "/projects" },
   { name: "Skills", link: "/skills" },
   { name: "About Me", link: "/about" },
@@ -19,52 +20,33 @@ export default function Navigation() {
   const pathname = usePathname();
   return (
     <header className="sticky top-0 flex h-20 items-center gap-4 px-4 md:px-8">
-      <div className="hidden w-40  flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+      <div className="hidden w-60  flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <BrandName />
       </div>
       <Sheet>
         <SheetTrigger asChild>
           <Button variant="outline" size="icon" className="shrink-0 md:hidden">
-            <s className="h-5 w-5" />
+            <Menu className="h-5 w-5" />
             <span className="sr-only">Toggle navigation menu</span>
           </Button>
         </SheetTrigger>
         <SheetContent side="left">
           <nav className="grid gap-6 text-lg font-medium">
-            <Link
-              href="#"
-              className="flex items-center gap-2 text-lg font-semibold"
-            >
-              <Package2 className="h-6 w-6" />
-              <span className="sr-only">Acme Inc</span>
-            </Link>
-            <Link href="#" className="hover:text-foreground">
-              Dashboard
-            </Link>
-            <Link
-              href="#"
-              className="text-muted-foreground hover:text-foreground"
-            >
-              Orders
-            </Link>
-            <Link
-              href="#"
-              className="text-muted-foreground hover:text-foreground"
-            >
-              Products
-            </Link>
-            <Link
-              href="#"
-              className="text-muted-foreground hover:text-foreground"
-            >
-              Customers
-            </Link>
-            <Link
-              href="#"
-              className="text-muted-foreground hover:text-foreground"
-            >
-              Analytics
-            </Link>
+            <BrandName />
+            {menus?.map((menu) => (
+              <Link
+                key={menu.link}
+                href={menu.link}
+                className={cn(
+                  "transition-colors hover:text-foreground",
+                  menu.link === pathname
+                    ? "text-foreground"
+                    : "text-muted-foreground"
+                )}
+              >
+                {menu.name}
+              </Link>
+            ))}
           </nav>
         </SheetContent>
       </Sheet>
