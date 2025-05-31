b0d40d31ca831d8b89004b71442706a9d3e52c2b
Faizan Adil <107530101+faizanibneadil@users.noreply.github.com>
2025-05-31 20:04:21 +0000

    chore: collection to slug param

--- src/app/(frontend)/c/[slug]/page.tsx
+++ src/app/(frontend)/c/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import type { CollectionSlug } from "payload";
 
-export default async function Collection(props: { params: Promise<{ collection: CollectionSlug }> }) {
-    return (await props.params).collection
+export default async function Collection(props: { params: Promise<{ slug: CollectionSlug }> }) {
+    return (await props.params).slug
 }
