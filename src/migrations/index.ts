e2bac9a798ebc9eadd95d8ee24ca85031748064e
Faizan Adil <107530101+faizanibneadil@users.noreply.github.com>
2025-07-04 22:46:59 +0000

    update: payloadcms versions and migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250704_213756 from './20250704_213756';
+import * as migration_20250704_224256 from './20250704_224256';
 
 export const migrations = [
   {
     up: migration_20250704_213756.up,
     down: migration_20250704_213756.down,
-    name: '20250704_213756'
+    name: '20250704_213756',
+  },
+  {
+    up: migration_20250704_224256.up,
+    down: migration_20250704_224256.down,
+    name: '20250704_224256'
   },
 ];
