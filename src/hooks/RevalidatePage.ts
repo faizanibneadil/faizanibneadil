b0857c5aaf495178054079928d84c6b144974cf4
Faizan Adil <faizanibneadil1@gmail.com>
2025-08-20 22:11:15 +0500

    type: generic hook type

--- src/hooks/RevalidatePage.ts
+++ src/hooks/RevalidatePage.ts
@@ -16,7 +16,9 @@ const getDomain = React.cache(async ({ payload, headers }: { payload: BasePayloa
     return tenant?.domain || null
 })
 
-export const RevalidatePageAfterChange: AppCollectionAfterChangeHook<Page> = ({ invalidateRootRoute }) => {
+export const RevalidatePageAfterChange: AppCollectionAfterChangeHook<Page, {
+    invalidateRootRoute?: boolean
+}> = ({ invalidateRootRoute }) => {
     return async ({
         doc,
         previousDoc,
@@ -49,35 +51,9 @@ export const RevalidatePageAfterChange: AppCollectionAfterChangeHook<Page> = ({ 
     }
 }
 
-// export const RevalidateRootPageAfterChange: CollectionAfterChangeHook<Page> = ({
-//     doc,
-//     previousDoc,
-//     req: { payload, context },
-// }) => {
-//     if (!context.disableRevalidate) {
-//         if (doc._status === 'published') {
-//             const path = doc.slug === 'home' ? '/' : `/${doc.slug}`
-
-//             payload.logger.info(`Revalidating page at path: ${path}`)
-
-//             revalidatePath(path)
-//             revalidateTag('pages-sitemap')
-//         }
-
-//         // If the page was previously published, we need to revalidate the old path
-//         if (previousDoc?._status === 'published' && doc._status !== 'published') {
-//             const oldPath = previousDoc.slug === 'home' ? '/' : `/${previousDoc.slug}`
-
-//             payload.logger.info(`Revalidating old page at path: ${oldPath}`)
-
-//             revalidatePath(oldPath)
-//             revalidateTag('pages-sitemap')
-//         }
-//     }
-//     return doc
-// }
-
-export const RevalidatePageAfterDelete: AppCollectionAfterDeleteHook = ({ invalidateRootRoute }) => {
+export const RevalidatePageAfterDelete: AppCollectionAfterDeleteHook<Page, {
+    invalidateRootRoute?: boolean
+}> = ({ invalidateRootRoute }) => {
     return async ({
         doc,
         req: { context, payload, headers },
@@ -99,12 +75,3 @@ export const RevalidatePageAfterDelete: AppCollectionAfterDeleteHook = ({ invali
         return doc
     }
 }
-// export const RevalidateRootPageAfterDelete: CollectionAfterDeleteHook<Page> = ({ doc, req: { context } }) => {
-//     if (!context.disableRevalidate) {
-//         const path = doc?.slug === 'home' ? '/' : `/${doc?.slug}`
-//         revalidatePath(path)
-//         revalidateTag('pages-sitemap')
-//     }
-
-//     return doc
-// }
