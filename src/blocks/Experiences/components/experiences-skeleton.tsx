1a586eeaccb6b73e48760bf7dc9c9282feab4e49
Faizan Adil <faizanibneadil1@google.com>
2025-12-28 18:01:46 +0500

    enhancements-v3

---
+++ src/blocks/Experiences/components/experiences-skeleton.tsx
@@ -0,0 +1,46 @@
+import { Card, CardHeader } from "@/components/ui/card"
+import { Skeleton } from "@/components/ui/skeleton"
+
+export async function ExperienceSkeleton() {
+    return (
+        <div className="flex min-h-0 flex-col gap-y-3">
+            <Skeleton className="w-60 h-4" />
+            {Array.from({ length: 5 }).map((item,idx) => (
+                <Card className="flex" key={`experience-skeleton-${idx}`}>
+                    <div className="flex-none">
+                        <Skeleton className="border size-12 m-auto bg-muted-background dark:bg-foreground rounded-full" />
+                    </div>
+                    <div className="flex-grow ml-4 items-center flex-col group">
+                        <CardHeader>
+                            <div className="flex items-start justify-between gap-x-2 text-base">
+                                <div className="flex flex-col items-start justify-start font-semibold leading-none text-xs sm:text-sm">
+                                    <Skeleton className="mb-1 w-40 h-2" />
+                                    <Skeleton className="mb-1 w-20 h-2" />
+                                    {/* <div className="flex flex-wrap gap-1 mt-1">
+                                        <h3 className="font-normal">TechStack: </h3>
+                                        {skillsFromProps?.map((skill, idx) => (
+                                            <React.Suspense key={`skill-${idx}`} fallback={<SkillSkeleton />}>
+                                                <Skill id={idx} skill={typeof skill === 'number' ? getSkillById({ id: skill }) : skill} />
+                                            </React.Suspense>
+                                        ))}
+                                    </div> */}
+                                </div>
+                                <div className="text-xs sm:text-sm tabular-nums text-muted-foreground text-right">
+                                    <Skeleton className="mb-1 w-20 h-2" />
+                                    <Skeleton className="mb-1 w-24 h-2" />
+                                    <Skeleton className="mb-1 w-12 h-2" />
+                                    <Skeleton className="mb-1 w-14 h-2" />
+                                </div>
+                            </div>
+                        </CardHeader>
+                        <Skeleton className="mb-1 w-10 h-2" />
+                        <Skeleton className="mb-1 w-60 h-2" />
+                        <Skeleton className="mb-1 w-full h-2" />
+                        <Skeleton className="mb-1 w-40 h-2" />
+                        <Skeleton className="mb-1 w-80 h-2" />
+                    </div>
+                </Card>
+            ))}
+        </div>
+    )
+}
--- src/blocks/index.tsx
+++ src/blocks/index.tsx
@@ -3,6 +3,7 @@ import dynamic from "next/dynamic";
 import { Page } from "@/payload-types";
 import { PagePropsWithParams } from "@/types";
 import type { BlockSlug } from "payload";
+import { Badge } from "@/components/ui/badge";
 
 // type PageLayout = NonNullable<Page['layout']>
 // type TBlocksSlugs = PageLayout extends (infer U)[]
@@ -17,64 +18,126 @@ import type { BlockSlug } from "payload";
 //}
 
 type TBlocks = {
-    [K in BlockSlug]?: React.ComponentType<{
-        params: PagePropsWithParams['params'],
-        blockProps: Extract<NonNullable<Page['layout']>[number], { blockType: K }>
-    }>
+    [K in BlockSlug]?: {
+        skeleton: React.ComponentType<{}>,
+        component: React.ComponentType<{
+            params: PagePropsWithParams['params'],
+            blockProps: Extract<NonNullable<Page['layout']>[number], { blockType: K }>
+        }>
+    }
 }
 
 const _blocks: TBlocks = {
-    hero: dynamic(() => import("@/blocks/Hero/components/hero").then(({ Hero }) => {
-        return Hero
-    })),
-    contact: dynamic(() => import("@/blocks/Contact/components/contact").then(({ Contact }) => {
-        return Contact
-    })),
-    education: dynamic(() => import("@/blocks/Education/components/educations").then(({ Education }) => {
-        return Education
-    })),
-    skill: dynamic(() => import("@/blocks/Skill/components/skills").then(({ Skill }) => {
-        return Skill
-    })),
-    experience: dynamic(() => import("@/blocks/Experiences/components/experiences").then(({ Experience }) => {
-        return Experience
-    })),
-    about: dynamic(() => import("@/blocks/About/components/about").then(({ About }) => {
-        return About
-    })),
-    hackathon: dynamic(() => import("@/blocks/Hackathon/components/hackathon").then(({ Hackathon }) => {
-        return Hackathon
-    })),
-    project: dynamic(() => import("@/blocks/Project/components/project").then(({ Project }) => {
-        return Project
-    })),
-    research: dynamic(() => import("@/blocks/Research/components/research").then(({ Research }) => {
-        return Research
-    })),
-    publication: dynamic(() => import("@/blocks/Publication/components/publication").then(({ Publication }) => {
-        return Publication
-    })),
-    license: dynamic(() => import("@/blocks/Licenses/components/license").then(({ License }) => {
-        return License
-    })),
-    certification: dynamic(() => import("@/blocks/Certification/components/certification").then(({ Certification }) => {
-        return Certification
-    })),
-    achievement: dynamic(() => import("@/blocks/Achievement/components/achievement").then(({ Achievement }) => {
-        return Achievement
-    })),
-    "github-contributions": dynamic(() => import("@/blocks/GithubContribution/components/github-contributions").then(({ GitHubContributionBlock }) => {
-        return GitHubContributionBlock
-    })),
-    "blogs-block": dynamic(() => import("@/blocks/Blogs/components/blogs").then(({ BlogsBlock }) => {
-        return BlogsBlock
-    })),
-    formBlock: dynamic(() => import("@/blocks/Form/components/form-block").then(({ FormBlock }) => {
-        return FormBlock
-    })),
-    "code-block": dynamic(() => import("@/blocks/Code/components/CodeBlock").then(({ CodeBlock }) => {
-        return CodeBlock
-    })),
+    hero: {
+        skeleton: dynamic(() => import("@/blocks/Hero/components/hero-skeleton").then(({ HeroSkeleton }) => {
+            return HeroSkeleton
+        })),
+        component: dynamic(() => import("@/blocks/Hero/components/hero").then(({ Hero }) => {
+            return Hero
+        }))
+    },
+    contact: {
+        component: dynamic(() => import("@/blocks/Contact/components/contact").then(({ Contact }) => {
+            return Contact
+        })),
+        skeleton: () => <div>Loading details ...</div>
+    },
+    education: {
+        component: dynamic(() => import("@/blocks/Education/components/educations").then(({ Education }) => {
+            return Education
+        })),
+        skeleton: dynamic(() => import("@/blocks/Education/components/educations-skeleton").then(({ EducationSkeleton }) => {
+            return EducationSkeleton
+        }))
+    },
+    skill: {
+        component: dynamic(() => import("@/blocks/Skill/components/skills").then(({ Skill }) => {
+            return Skill
+        })),
+        skeleton: () => <div>Loading skills details ...</div>
+    },
+    experience: {
+        component: dynamic(() => import("@/blocks/Experiences/components/experiences").then(({ Experience }) => {
+            return Experience
+        })),
+        skeleton: dynamic(() => import("@/blocks/Experiences/components/experiences-skeleton").then(({ ExperienceSkeleton }) => {
+            return ExperienceSkeleton
+        }))
+    },
+    about: {
+        component: dynamic(() => import("@/blocks/About/components/about").then(({ About }) => {
+            return About
+        })),
+        skeleton: () => <div>Loading about details ...</div>
+    },
+    hackathon: {
+        component: dynamic(() => import("@/blocks/Hackathon/components/hackathon").then(({ Hackathon }) => {
+            return Hackathon
+        })),
+        skeleton: () => <div>Loading hackathon details ...</div>
+    },
+    project: {
+        component: dynamic(() => import("@/blocks/Project/components/project").then(({ Project }) => {
+            return Project
+        })),
+        skeleton: () => <div>Loading projects Details ...</div>
+    },
+    research: {
+        component: dynamic(() => import("@/blocks/Research/components/research").then(({ Research }) => {
+            return Research
+        })),
+        skeleton: () => <div>Loading researches details ...</div>
+    },
+    publication: {
+        component: dynamic(() => import("@/blocks/Publication/components/publication").then(({ Publication }) => {
+            return Publication
+        })),
+        skeleton: () => <div>Loading publications details ...</div>
+    },
+    license: {
+        component: dynamic(() => import("@/blocks/Licenses/components/license").then(({ License }) => {
+            return License
+        })),
+        skeleton: () => <div>Loading license details ....</div>
+    },
+    certification: {
+        component: dynamic(() => import("@/blocks/Certification/components/certification").then(({ Certification }) => {
+            return Certification
+        })),
+        skeleton: () => <div>Loading certifications details ...</div>
+    },
+    achievement: {
+        component: dynamic(() => import("@/blocks/Achievement/components/achievement").then(({ Achievement }) => {
+            return Achievement
+        })),
+        skeleton: () => <div>Loading achievement details ...</div>
+    },
+    "github-contributions": {
+        component: dynamic(() => import("@/blocks/GithubContribution/components/github-contributions").then(({ GitHubContributionBlock }) => {
+            return GitHubContributionBlock
+        })),
+        skeleton: dynamic(() => import("@/blocks/GithubContribution/components/github-contributions-skeleton").then(({ GithubContributionsSkeleton }) => {
+            return GithubContributionsSkeleton
+        }))
+    },
+    "blogs-block": {
+        component: dynamic(() => import("@/blocks/Blogs/components/blogs").then(({ BlogsBlock }) => {
+            return BlogsBlock
+        })),
+        skeleton: () => <div>Loading blogs details ...</div>
+    },
+    formBlock: {
+        component: dynamic(() => import("@/blocks/Form/components/form-block").then(({ FormBlock }) => {
+            return FormBlock
+        })),
+        skeleton: () => <div>Loading form details ...</div>
+    },
+    "code-block": {
+        component: dynamic(() => import("@/blocks/Code/components/CodeBlock").then(({ CodeBlock }) => {
+            return CodeBlock
+        })),
+        skeleton: () => <div>Loading code details ...</div>
+    },
 }
 
 export function BlocksRenderer(props: { blocks: Page['layout'][][0], params: PagePropsWithParams['params'] }) {
@@ -85,16 +148,23 @@ export function BlocksRenderer(props: { blocks: Page['layout'][][0], params: Pag
     if (hasBlocks) {
         return (
             <Fragment>
+                <section id="contact" className="relative mx-auto flex w-full max-w-3xl flex-col justify-between gap-y-6 bg-secondary/80 py-2 dark:bg-secondary/40 -mt-10">
+                    {/* <div className="-translate-x-1/2 -top-px pointer-events-none absolute left-1/2 w-screen border-t" /> */}
+                    <a href='https://skillshelf.vercel.app' className="text-xs">Powered by skillshelf.vercel.app</a>
+                    <div className="-translate-x-1/2 -bottom-px pointer-events-none absolute left-1/2 w-screen border-b" />
+                </section>
+
                 {blocks.map((block, index) => {
                     const { blockType } = block
 
                     if (blockType && blockType in _blocks) {
-                        const Block = _blocks[blockType]
+                        const Block = _blocks[blockType]?.component
+                        const Skeleton = _blocks[blockType]?.skeleton!
 
                         if (Block) {
                             return (
-                                <div className="my-5" key={`${blockType}-${index}`}>
-                                    <React.Suspense fallback='Loading...'>
+                                <div className="my-5 first:mt-0" key={`${blockType}-${index}`}>
+                                    <React.Suspense fallback={<Skeleton />}>
                                         {/* @ts-expect-error there may be some mismatch between the expected types here */}
                                         <Block blockProps={block} params={params} />
                                     </React.Suspense>
