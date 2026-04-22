2c12891bf23ff185c588faef4ce5f81f49d54bac
Faizan Adil <faizanibneadil1@google.com>
2026-04-23 00:47:08 +0500

    migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260421_164344 from './20260421_164344';
 import * as migration_20260422_072419 from './20260422_072419';
+import * as migration_20260422_194609 from './20260422_194609';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260422_072419.up,
     down: migration_20260422_072419.down,
-    name: '20260422_072419'
+    name: '20260422_072419',
+  },
+  {
+    up: migration_20260422_194609.up,
+    down: migration_20260422_194609.down,
+    name: '20260422_194609'
   },
 ];
